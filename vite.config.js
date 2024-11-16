3c75b773a3d446c2526fa9472baf02f3e8fb3ba0
Kari Wu <22714241+kari1358@users.noreply.github.com>
2024-11-15 21:28:55 -0800

    Add google maps api and gameplay, has errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    open: true, // Automatically opens the browser
-  },
+  // Your configurations
 });
